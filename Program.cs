9715fb56d91249c518ba07d3bb7047febe701d57
Ghazalyyy <121621333+Ghazalyyy@users.noreply.github.com>
2022-12-30 15:10:37 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,35 +1,32 @@
-﻿using System;
-
-namespace PRIME_NUMB1
+﻿    using System;
+namespace PERFECT_NUMB_2
 {
-	 class MainClass {
+    internal class PerfectNumb
 
-		static bool isPrime(int x)
-		{
+    {
+        static void Main()
+        {
+            Console.WriteLine("" /***** program - Perfect Numbers From 1 to 100 *****/ );
+            int number, sum0fNumbers = 0, i = 1;
+            Console.WriteLine("Perfect Numbers From 1 to 100 are: ");
+            for (number = 1; number <= 100; number++)
+            {
+                i = 1;
+                sum0fNumbers = 0;
+                while (i < number)
+                {
+                    if (number % i == 0)
+                        sum0fNumbers =
+                         sum0fNumbers + i;
+                    i++;
+                }
+                if (sum0fNumbers == number)
+                {
+                    Console.WriteLine("{0} is a Perfect Number", number);
+                }
+            }
+            Console.ReadLine();
 
-			if (x == 2) return true;
-			if (x == 3) return true;
-
-
-			for (int i = 2; i < 5; i++)
-			{
-				if (x % i == 0) return false;
-
-			}
-			return true;
-		}
-
-		static void Main(string[] args)
-		{
-
-			int i = 2;
-			while (i <= 100)
-			{
-				if (isPrime(i))
-					Console.Write(i + " ");
-				i++;
-			}
-
-		}
-	}
+        }
+    }
 }
