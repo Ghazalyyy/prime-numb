fa5c9a30be89638e70948c03eece3c6b22e3485c
Ghazalyyy <121621333+Ghazalyyy@users.noreply.github.com>
2022-12-30 14:56:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PRIME_NUMB1
+{
+	 class MainClass {
+
+		static bool isPrime(int x)
+		{
+
+			if (x == 2) return true;
+			if (x == 3) return true;
+
+
+			for (int i = 2; i < 5; i++)
+			{
+				if (x % i == 0) return false;
+
+			}
+			return true;
+		}
+
+		static void Main(string[] args)
+		{
+
+			int i = 2;
+			while (i <= 100)
+			{
+				if (isPrime(i))
+					Console.Write(i + " ");
+				i++;
+			}
+
+		}
+	}
+}
